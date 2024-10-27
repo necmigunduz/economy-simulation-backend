bd31bb0242227334c360f2b445a89fd36a2e4214
Necmi Gunduz <necmigunduz@gmail.com>
2024-10-27 18:14:51 +0300

    add: config file for MongoDB

---
+++ config/db.js
@@ -0,0 +1,18 @@
+// config.js
+const mongoose = require('mongoose') 
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+
+        console.log("MongoDB connected!")
+    } catch (error) {
+        console.log(error.message)
+        process.exit(1)
+    }
+}
+
+mongoose.exports = connectDB
