22ae5221b3d12fc2210a439f2540e24c203575b5
Necmi Gunduz <necmigunduz@gmail.com>
2024-10-28 18:57:16 +0300

    complete: register

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,8 +4,14 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 
 exports.register = async (req, res) => {
-    const { name, email, password } = req.body
-    const hashedPassword = await bcrypt.hash(password, 10)
-    const user = new User({name, email, password: hashedPassword})
-    
- }
+    try {
+        const { name, email, password } = req.body
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const user = new User({ name, email, password: hashedPassword })
+
+        await user.save()
+        res.status(201).json({ message: 'User created successfully!' })
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+}
