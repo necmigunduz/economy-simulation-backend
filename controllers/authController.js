0c98bf46a32f45c012779927818568a7a0832828
Necmi Gunduz <necmigunduz@gmail.com>
2024-10-28 18:55:26 +0300

    add: add register controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,3 +2,10 @@
 const User = require('../models/User')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
+
+exports.register = async (req, res) => {
+    const { name, email, password } = req.body
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const user = new User({name, email, password: hashedPassword})
+    
+ }
