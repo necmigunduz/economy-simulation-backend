1d4064f2d9c254d1a9f385dad6c7ef96b45eccee
Necmi Gunduz <necmigunduz@gmail.com>
2024-10-30 09:49:14 +0300

    complete: middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,8 +5,10 @@ module.exports = (req, res, next) => {
     if (!token) return res.status(401).json({ message: 'No token, authorization is denied!'})
     
     try {
-        
+        const decoded = jwt.verify( token, process.env.JWT_SECRET )
+        req.user = decoded.userId
+        next()
     } catch (error) {
-        
+        res.status(401).json({ error: 'Token is not valid.' + ' ' + error.message })
     }
 }
